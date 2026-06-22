fbf12f7f40d3ea20e3d733734b7b887f9cc293bb
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-06-23 00:05:43 +0200

    Update layout.tsx

--- app/[username]/layout.tsx
+++ app/[username]/layout.tsx
@@ -1,17 +1,21 @@
 import { ProtectedRoute } from '@/components/auth/ProtectedRoute';
 import { Navbar } from '@/components/layout/Navbar';
 import { Sidebar } from '@/components/layout/Sidebar';
+import { Footer } from '@/components/layout/Footer';
 
 export const dynamic = 'force-dynamic';
 
 export default function UsernameLayout({ children }: { children: React.ReactNode }) {
   return (
     <ProtectedRoute>
-      <div className="min-h-screen">
+      <div className="min-h-screen flex flex-col">
         <Navbar />
-        <div className="flex">
+        <div className="flex flex-1">
           <Sidebar />
-          <main className="flex-1 p-6 overflow-auto">{children}</main>
+          <main className="flex-1 p-6 overflow-auto flex flex-col">
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </main>
         </div>
       </div>
     </ProtectedRoute>
